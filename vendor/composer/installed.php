b0ea44751cdff12b3113e8aef3590d781aa96362
Amir Sanni <amirsanni@gmail.com>
2022-10-27 18:51:03 +0100

    Add vendor to VC

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'amirsanni/php-ews-wrapper',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ad22de2681d5b08726e7104f64ac674cb360b76a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'amirsanni/php-ews-wrapper' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ad22de2681d5b08726e7104f64ac674cb360b76a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'php-ews/php-ews' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '8b3514b562570a239a5cbcbc13af70f97abda553',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../php-ews/php-ews',
+            'aliases' => array(
+                0 => '1.0.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
